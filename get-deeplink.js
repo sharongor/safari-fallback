16f9944a0293e54c55dabe71ea7acfaed5f61c11
Sharon Gordon <sharong@raynw.com>
2025-01-29 12:19:19 +0200

    Adding fetch bypass

--- get-deeplink.js
+++ get-deeplink.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json({
-        deepLink: "https://www.fnx.co.il" 
+        deepLink: "https://pinfo-mobileapp-services.fnx.co.il/onelink/navigate/TreatmentSubjectsScreen" 
     });
 }
