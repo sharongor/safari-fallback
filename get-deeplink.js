71a6a38087c966aa22a4513988d629da0808fd82
Sharon Gordon <sharong@raynw.com>
2025-01-29 09:58:22 +0200

    Return to Deeplink

--- get-deeplink.js
+++ get-deeplink.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json({
-        deepLink: "https://www.fnx.co.il/pinfoapp" 
+        deepLink: "https://www.fnx.co.il" 
     });
 }
